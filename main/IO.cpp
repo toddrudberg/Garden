61e47c04b222e7826ad0b4170de56867522836ef
toddr <todd.rudberg@icloud.com>
2024-05-04 18:50:26 -0700

    some comments

--- main/IO.cpp
+++ main/IO.cpp
@@ -4,6 +4,9 @@ const int rs485rX = 0;  //Definition RS485 shield RX terminal
 const int rs485tX = 1;  //Definition RS485 shield TX terminal
 const int rs485TxEnable = 2;  //Definition RS485 shield enable terminal 
 const unsigned int sdChipSelect = 10; // SD card chip select
+//11 SPI MOSI
+//12 SPI MISO
+//13 SPI SCK
 
 const unsigned long baud = 9600;
 SoftwareSerial mySerial(rs485rX, rs485tX); // RX, TX
