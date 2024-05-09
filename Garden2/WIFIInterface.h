761918f941f85f2327a7dc04c074e95c3d8929db
toddrudberg <todd.rudberg@icloud.com>
2024-05-08 21:42:42 -0700

    initial push

---
+++ Garden2/WIFIInterface.h
@@ -0,0 +1,24 @@
+#ifndef WIFIINTERFACE_H
+#define WIFIINTERFACE_H
+#include "IO.h"
+#include <WiFi.h>
+#include <NTPClient.h>
+
+class cWIFIInterface
+{
+public:
+    cWIFIInterface();
+    bool setupWIFI();
+    void checkWIFI(sSoilSensorData* soilSensorData);
+    void runWIFI(sSoilSensorData* soilSensorData);
+    void CheckNtpTime();
+
+private:
+    WiFiUDP ntpUDP;
+    NTPClient timeClient;
+    const char* ssid = "CenturyLink2286";
+    const char* password = "vvgs3nsmrw2549";
+    WiFiServer server;
+};
+
+#endif // WIFIINTERFACE_H
